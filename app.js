496c0635710e734f4fa3b2ae9834df043299a1f2
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-12-17 15:19:06 +0200

    os module

--- app.js
+++ app.js
@@ -3,9 +3,15 @@
  // logging module
   looger.message('raissa')
 */
-// path module
+/* path module
 const path = require('path');
 var pathObj = path.parse(__filename)
 console.log(pathObj)
-
-//OS module
+*/
+/*OS module
+const os = require('os');
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+console.log(`total memory:${totalMemory}`);
+console.log(`free memory:${freeMemory}`);
+*/
