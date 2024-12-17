adc64e6849e337056a5dd6a452fd30e713742f2d
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-12-17 15:07:56 +0200

    path module

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-// loading a module 
-var looger = require('./looger');
-console.log(looger);
+/* // loading a module 
+ const looger = require('./looger');
+ // logging module
+  looger.message('raissa')
+*/
+// path module
+const path = require('path');
+var pathObj = path.parse(__filename)
+console.log(pathObj)
+
+//OS module
